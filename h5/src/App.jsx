c0a4cac3b803cd6909cb743354ea2923eb580b4e
住好房 <admin@zhuhaofang.com>
2026-06-23 10:51:07 +0800

    fix: H5捏合缩放修复(Swiper阻断了手势) + 双击缩放还原

--- h5/src/App.jsx
+++ h5/src/App.jsx
@@ -1,11 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
 import { ToastProvider } from './components/Toast';
+import useDoubleTapZoom from './hooks/useDoubleTapZoom';
 import router from './router';
+
+function AppInner() {
+  useDoubleTapZoom();
+  return <RouterProvider router={router} />;
+}
 
 function App() {
   return (
     <ToastProvider>
-      <RouterProvider router={router} />
+      <AppInner />
     </ToastProvider>
   );
 }
